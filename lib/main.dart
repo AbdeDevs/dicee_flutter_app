bc4cc3a657757e387dca44884dd6e420357b7530
Abdellah Lamrabat <udlunieps@gmail.com>
2024-12-21 18:32:28 +0100

    feat: add design app

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        elevation: 20.0,
-        title: Text(
-          'Dicee',
-          style: TextStyle(fontSize: 25.0, color: Colors.white),
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          elevation: 20.0,
+          shadowColor: Colors.lightBlue,
+          title: const Text(
+            'Dicee',
+            style: TextStyle(fontSize: 25.0, color: Colors.white),
+          ),
         ),
+        body: DiceePage(),
       ),
-      body: DiceePage(),
-    ));
+    );
   }
 }
 
 class DiceePage extends StatelessWidget {
+  const DiceePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
