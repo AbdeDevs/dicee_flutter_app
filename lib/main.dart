264c32d7c45aefb317e609b1b1f531d16b5e2e60
Abdellah Lamrabat <udlunieps@gmail.com>
2024-12-21 17:10:06 +0100

    feat: add AppBar with title and visually separate it from body

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        elevation: 20.0,
+        title: Text(
+          'Dicee',
+          style: TextStyle(fontSize: 25.0, color: Colors.white),
+        ),
+      ),
+      body: DiceePage(),
+    ));
+  }
+}
+
+class DiceePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
   }
 }
